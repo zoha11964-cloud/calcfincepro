69f60b24565e82c39c5ab22e80ddc1b2b767a7ba
zoha11964-cloud <zoha11964@gmail.com>
2026-06-03 15:21:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11093766
+    "domain": "3nbf4.com",
+    "zoneId": 11093800
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
